0c1c28b5829681f167f78246e2e213176ea94519
goshbello <44555654+goshbello@users.noreply.github.com>
2022-03-21 00:35:51 +0000

    resolving email delivery issue in production

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative "application"
 Rails.application.initialize!
 
 # setting for account email verification. This is general settings. see config/enviroment/development and production for addional settings
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+
+ActionMailer::Base.smtp_settings = {
   address:             'smtp.gmail.com',
   port:                 587,
   authentication:      'plain',
